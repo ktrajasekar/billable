2db9f42d1e14f240ab7d6f9c307b8874f820ef96
Rajasekar T <kt.rajasekar@gmail.com>
2022-06-30 02:59:39 +0530

    added view for invoice

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Invoice from '../views/Invoice.vue'
 
 Vue.use(VueRouter)
 
@@ -8,7 +8,7 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Invoice
   },
   {
     path: '/about',
---
+++ src/views/Invoice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="invoice-page">
+      <h1>INVOICE</h1>
+    </div>
+    <section class="company-profile">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-7">
+            <div class="focus-editor" contenteditable>
+                <h2>COMPANY NAME</h2>
+            </div>
+          </div>
+            <div class="col-sm-5">
+                <div contenteditable  class="focus-editor">
+                    sada
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
